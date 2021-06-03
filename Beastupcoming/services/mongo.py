145678174958b72fb91f3fb35fa9de206df3c8c4
SÀRKÀR <85276360+Sarkarthelegend@users.noreply.github.com>
2021-06-04 00:37:08 +0530

    Update mongo.py

--- Beastupcoming/services/mongo.py
+++ Beastupcoming/services/mongo.py
@@ -21,8 +21,8 @@ from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
-from DaisyX import log
-from DaisyX.config import get_int_key, get_str_key
+from beastupcoming import log
+from beastupcoming.config import get_int_key, get_str_key
 
 MONGO_URI = get_str_key("MONGO_URI")
 MONGO_PORT = get_int_key("MONGO_PORT")
