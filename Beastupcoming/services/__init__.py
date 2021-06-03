f2bcc7da4f95d6213e77ed0cc19d466bd154f2ea
SÀRKÀR <85276360+Sarkarthelegend@users.noreply.github.com>
2021-06-03 22:47:43 +0530

    Update and rename __init__.py to Beastupcoming/services/__init__.py

--- Beastupcoming/services/__init__.py
+++ Beastupcoming/services/__init__.py
@@ -1,4 +1,4 @@
-# This file is part of Daisy (Telegram Bot)
+# This file is part of Beastupcoming (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
