424a090819921b305e5be7534c036022657ee3dd
SÀRKÀR <85276360+Sarkarthelegend@users.noreply.github.com>
2021-06-04 00:19:59 +0530

    Update json.py

--- Beastupcoming/module/json.py
+++ Beastupcoming/module/json.py
@@ -22,8 +22,8 @@ from telethon import types
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
-from Beastupcoming.services.events import register
-from Beastupcoming.services.telethon import tbot as borg
+from beastupcoming.services.events import register
+from beastupcoming.services.telethon import tbot as borg
 
 
 async def is_register_admin(chat, user):
