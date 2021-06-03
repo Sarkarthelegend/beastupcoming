791312deb2c53ecc141582c8c6f40e03f1ef6bea
SÀRKÀR <85276360+Sarkarthelegend@users.noreply.github.com>
2021-06-04 00:16:00 +0530

    Update telethonbasics.py

--- Beastupcoming/function/telethonbasics.py
+++ Beastupcoming/function/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from Beastupcoming.function.pluginhelpers import runcmd
+from beastupcoming.function.pluginhelpers import runcmd
 
 
 async def convert_to_image(event, borg):
