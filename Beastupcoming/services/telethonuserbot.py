46c86f9b1a8a4f537bebcfc2b741fe81d86a2095
SÀRKÀR <85276360+Sarkarthelegend@users.noreply.github.com>
2021-06-04 00:42:09 +0530

    Update telethonuserbot.py

--- Beastupcoming/services/telethonuserbot.py
+++ Beastupcoming/services/telethonuserbot.py
@@ -18,7 +18,7 @@ import sys
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-from DaisyX.config import get_int_key, get_str_key
+from beastupcoming.config import get_int_key, get_str_key
 
 STRING_SESSION = get_str_key("STRING_SESSION", required=True)
 API_ID = get_int_key("APP_ID", required=True)
